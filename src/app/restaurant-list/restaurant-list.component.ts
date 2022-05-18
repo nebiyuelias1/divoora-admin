b5bfd60883af2ece5f91a47afee113bff1c77020
Nathan Getachew <nathygech111@gmail.com>
2022-05-18 10:24:28 +0300

    added restaurants dummy data

--- src/app/restaurant-list/restaurant-list.component.ts
+++ src/app/restaurant-list/restaurant-list.component.ts
@@ -7,9 +7,111 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantListComponent implements OnInit {
 
+  allRestaurants: Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.allRestaurants = [
+            {
+              id: '#194356',
+              name: "McDonald's Pazzallo",
+              catchPhrase: "I'm Lovin'it",
+              address: 'Via la Sguancia 9, 6912 Lugano',
+              price: '$149',
+            },
+            {
+              id: '#323323',
+              name: 'Burger King',
+              catchPhrase: 'Home of the Whopper',
+              address: 'Piazza Alessandro Manzoni 1, 6900 Lugano',
+              price: '$129',
+            },
+            {
+              id: '#194356',
+              name: "McDonald's Pazzallo",
+              catchPhrase: "I'm Lovin'it",
+              address: 'Via la Sguancia 9, 6912 Lugano',
+              price: '$149',
+            },
+            {
+              id: '#323323',
+              name: 'Burger King',
+              catchPhrase: 'Home of the Whopper',
+              address: 'Piazza Alessandro Manzoni 1, 6900 Lugano',
+              price: '$129',
+            },
+            {
+              id: '#194356',
+              name: "McDonald's Pazzallo",
+              catchPhrase: "I'm Lovin'it",
+              address: 'Via la Sguancia 9, 6912 Lugano',
+              price: '$149',
+            },
+            {
+              id: '#323323',
+              name: 'Burger King',
+              catchPhrase: 'Home of the Whopper',
+              address: 'Piazza Alessandro Manzoni 1, 6900 Lugano',
+              price: '$129',
+            },
+            {
+              id: '#194356',
+              name: "McDonald's Pazzallo",
+              catchPhrase: "I'm Lovin'it",
+              address: 'Via la Sguancia 9, 6912 Lugano',
+              price: '$149',
+            },
+            {
+              id: '#323323',
+              name: 'Burger King',
+              catchPhrase: 'Home of the Whopper',
+              address: 'Piazza Alessandro Manzoni 1, 6900 Lugano',
+              price: '$129',
+            },
+            {
+              id: '#194356',
+              name: "McDonald's Pazzallo",
+              catchPhrase: "I'm Lovin'it",
+              address: 'Via la Sguancia 9, 6912 Lugano',
+              price: '$149',
+            },
+            {
+              id: '#323323',
+              name: 'Burger King',
+              catchPhrase: 'Home of the Whopper',
+              address: 'Piazza Alessandro Manzoni 1, 6900 Lugano',
+              price: '$129',
+            },
+            {
+              id: '#194356',
+              name: "McDonald's Pazzallo",
+              catchPhrase: "I'm Lovin'it",
+              address: 'Via la Sguancia 9, 6912 Lugano',
+              price: '$149',
+            },
+            {
+              id: '#323323',
+              name: 'Burger King',
+              catchPhrase: 'Home of the Whopper',
+              address: 'Piazza Alessandro Manzoni 1, 6900 Lugano',
+              price: '$129',
+            },
+            {
+              id: '#194356',
+              name: "McDonald's Pazzallo",
+              catchPhrase: "I'm Lovin'it",
+              address: 'Via la Sguancia 9, 6912 Lugano',
+              price: '$149',
+            },
+            {
+              id: '#323323',
+              name: 'Burger King',
+              catchPhrase: 'Home of the Whopper',
+              address: 'Piazza Alessandro Manzoni 1, 6900 Lugano',
+              price: '$129',
+            },
+        ];
   }
 
 }
